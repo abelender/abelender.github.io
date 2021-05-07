488d019b757b4a91c52947f369553420c601beae
Jose M Martinez <jonuel.martiguez@gmail.com>
2021-05-08 01:44:25 +0200

    druid restoration tree talent icons added

--- druid/druidrestoration.js
+++ druid/druidrestoration.js
@@ -1,8 +1,38 @@
 let druidrestoration = [
 
     {   
-        name: 'Starlight Wrath',
-        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_abolishmagic.jpg',
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: true,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Improved Mark of the Wild",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_regeneration.jpg',
+        pointSpent: 0,
+        pointLimit: 5,
+        isEnable: true,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Furor",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_holy_blessingofstamina.jpg',
         pointSpent: 0,
         pointLimit: 5,
         isEnable: true,
@@ -31,12 +61,387 @@ let druidrestoration = [
     },
 
     {   
+        name: "Naturalist",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_healingtouch.jpg',
+        pointSpent: 0,
+        pointLimit: 5,
+        isEnable: false,
+        lineNumber: 2,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Nature's Focus",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_healingwavegreater.jpg',
+        pointSpent: 0,
+        pointLimit: 5,
+        isEnable: false,
+        lineNumber: 2,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Natural Shapeshifter",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_wispsplode.jpg',
+        pointSpent: 0,
+        pointLimit: 3,
+        isEnable: false,
+        lineNumber: 2,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
         name: undefined,
         src: undefined,
         pointSpent: undefined,
         pointLimit: undefined,
         isEnable: false,
         lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Intensity",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_frost_windwalkon.jpg',
+        pointSpent: 0,
+        pointLimit: 3,
+        isEnable: false,
+        lineNumber: 3,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Subtlety",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/ability_eyeoftheowl.jpg',
+        pointSpent: 0,
+        pointLimit: 5,
+        isEnable: false,
+        lineNumber: 3,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Omen of Clarity",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_crystalball.jpg',
+        pointSpent: 0,
+        pointLimit: 1,
+        isEnable: false,
+        lineNumber: 3,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Tranquil Spirit",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_holy_elunesgrace.jpg',
+        pointSpent: 0,
+        pointLimit: 5,
+        isEnable: false,
+        lineNumber: 4,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Improved Rejuvenation",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_rejuvenation.jpg',
+        pointSpent: 0,
+        pointLimit: 3,
+        isEnable: false,
+        lineNumber: 4,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Nature's Swiftness",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_ravenform.jpg',
+        pointSpent: 0,
+        pointLimit: 1,
+        isEnable: false,
+        lineNumber: 5,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Gift of Nature",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_protectionformnature.jpg',
+        pointSpent: 0,
+        pointLimit: 5,
+        isEnable: false,
+        lineNumber: 5,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Improved Tranquility",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_tranquility.jpg',
+        pointSpent: 0,
+        pointLimit: 2,
+        isEnable: false,
+        lineNumber: 5,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Empowered Touch",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/ability_druid_empoweredtouch.jpg',
+        pointSpent: 0,
+        pointLimit: 2,
+        isEnable: false,
+        lineNumber: 6,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Improved Regrowth",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_resistnature.jpg',
+        pointSpent: 0,
+        pointLimit: 5,
+        isEnable: false,
+        lineNumber: 6,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Living Spirit",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/spell_nature_giftofthewaterspirit.jpg',
+        pointSpent: 0,
+        pointLimit: 3,
+        isEnable: false,
+        lineNumber: 7,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Swiftmend",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/inv_relics_idolofrejuvenation.jpg',
+        pointSpent: 0,
+        pointLimit: 1,
+        isEnable: false,
+        lineNumber: 7,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Natural Perfection",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/ability_druid_naturalperfection.jpg',
+        pointSpent: 0,
+        pointLimit: 3,
+        isEnable: false,
+        lineNumber: 7,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+     
+    },
+
+    {   
+        name: "Empowered Rejuvenation",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/ability_druid_empoweredrejuvination.jpg',
+        pointSpent: 0,
+        pointLimit: 5,
+        isEnable: false,
+        lineNumber: 8,
         rankText: {
 
         },
@@ -91,417 +496,12 @@ let druidrestoration = [
     },
 
     {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
+        name: "Tree of Life",
+        src: 'https://wow.zamimg.com/images/wow/icons/large/ability_druid_treeoflife.jpg',
+        pointSpent: 0,
+        pointLimit: 1,
         isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
-        rankText: {
-
-        },
-        isTalent: false,
-        addRequirement: function() { return ""}
-     
-    },
-
-    {   
-        name: undefined,
-        src: undefined,
-        pointSpent: undefined,
-        pointLimit: undefined,
-        isEnable: false,
-        lineNumber: 1,
+        lineNumber: 9,
         rankText: {
 
         },
