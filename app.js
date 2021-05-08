ccf4945eb0cb48ec464cab88da8e7d2135cdaacf
Jose M Martinez <jonuel.martiguez@gmail.com>
2021-05-08 15:48:24 +0200

    hunter object files created

--- app.js
+++ app.js
@@ -154,6 +154,76 @@ function createDruid () {
 
 }
 
+function createHunter () {    
+
+    if(document.getElementById('globalwrapper')) {
+
+        document.getElementById('globalwrapper').remove();
+
+        for(i = 0; i <= 35; i++){
+
+            if(druidbalance[i].name !== undefined) {
+               druidbalance[i].pointSpent = 0;
+            }
+
+            if(druidferal[i].name !== undefined) {
+                druidferal[i].pointSpent = 0;
+            }
+
+            if(druidrestoration[i].name !== undefined) {
+                druidrestoration[i].pointSpent = 0;
+            }
+            
+        }
+        globalTalentLimit = 0
+    }
+    
+    let divGlobalWrapper = document.createElement('div');
+        divGlobalWrapper.className = 'global-wrapper';
+        divGlobalWrapper.id = "globalwrapper"; 
+    
+    let divTalentTreeWrapper = document.createElement('div');
+        divTalentTreeWrapper.className = 'talent-tree-wrapper';
+        divTalentTreeWrapper.id = "treewrapper"; 
+
+
+
+     globalSpanPoints(divGlobalWrapper);
+     sharedLink( addClassToURL('druid'), divGlobalWrapper);
+
+
+        talentTreeBuilder(druidbalance, 
+                        'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d502015c-36dd-49db-ba9d-9282d94d795a/db2i6k2-15620ca8-b438-41b9-847f-86bf0b675e1d.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q1MDIwMTVjLTM2ZGQtNDlkYi1iYTlkLTkyODJkOTRkNzk1YVwvZGIyaTZrMi0xNTYyMGNhOC1iNDM4LTQxYjktODQ3Zi04NmJmMGI2NzVlMWQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.3-NcH41QoewdDuV97kk5ejjl-gumR-rnLla8ZWCs9aM', 
+                        'Balance', 
+                        'https://wow.zamimg.com/images/wow/icons/large/spell_nature_starfall.jpg', divTalentTreeWrapper, 'A');
+
+        talentTreeBuilder(druidferal, 
+                        'https://cdnb.artstation.com/p/assets/images/images/027/550/751/large/ilya-generalov-pixiedust.jpg?1591853187', 
+                        'Feral', 
+                        'https://wow.zamimg.com/images/wow/icons/large/ability_racial_bearform.jpg', divTalentTreeWrapper, 'B');
+
+        talentTreeBuilder(druidrestoration, 
+                        'https://worldofhealcraft.files.wordpress.com/2014/10/tree-of-life-2.jpg', 
+                        'Restoration', 
+                        'https://wow.zamimg.com/images/wow/icons/large/spell_nature_healingtouch.jpg', divTalentTreeWrapper, 'C');
+
+
+
+        divGlobalWrapper.appendChild(divTalentTreeWrapper);                
+        document.body.appendChild(divGlobalWrapper);
+
+        let url = new URL(window.location.href);
+
+        if(url.searchParams.get('class') == 'druid') {
+            
+            buildTree(divTalentTreeWrapper, url);
+
+        }
+
+        
+
+}
+
 
 function addClassToURL(classname) {
    
---
+++ hunter/hunterbeastmastery.js
@@ -0,0 +1,542 @@
+let hunterbeastmastery = [
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 1,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 2,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 2,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 2,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 2,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 3,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 3,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 3,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 3,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 4,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 4,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 4,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 4,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 5,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 5,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 5,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 5,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 6,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 6,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 6,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 6,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 7,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 7,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 7,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 7,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 8,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 8,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 8,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 8,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 9,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 9,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 9,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    },
+
+    {   
+        name: undefined,
+        src: undefined,
+        pointSpent: undefined,
+        pointLimit: undefined,
+        isEnable: false,
+        lineNumber: 9,
+        rankText: {
+
+        },
+        isTalent: false,
+        addRequirement: function() { return ""}
+        
+    }
+]
