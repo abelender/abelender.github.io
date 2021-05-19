599457b0256740e523de64b0f2bdaf3cd49085a2
Jose M Martinez <jonuel.martiguez@gmail.com>
2021-05-20 01:29:15 +0200

    priest talent icons info added

--- priest/priestholy.js
+++ priest/priestholy.js
@@ -8,7 +8,8 @@ let priestholy = [
         isEnable: true,
         lineNumber: 1,
         rankText: {
-
+            1: "Gives you a 35% chance to avoid interruption caused by damage while casting any healing spell.",
+            2: "Gives you a 70% chance to avoid interruption caused by damage while casting any healing spell."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -23,7 +24,9 @@ let priestholy = [
         isEnable: true,
         lineNumber: 1,
         rankText: {
-
+            1: "Increases the amount healed by your Renew spell by 5%.",
+            2: "Increases the amount healed by your Renew spell by 10%.",
+            3: "Increases the amount healed by your Renew spell by 15%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -38,7 +41,11 @@ let priestholy = [
         isEnable: true,
         lineNumber: 1,
         rankText: {
-
+            1: "Increases the critical effect chance of your Holy spells by 1%.",
+            2: "Increases the critical effect chance of your Holy spells by 2%.",
+            3: "Increases the critical effect chance of your Holy spells by 3%.",
+            4: "Increases the critical effect chance of your Holy spells by 4%.",
+            5: "Increases the critical effect chance of your Holy spells by 5%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -83,7 +90,11 @@ let priestholy = [
         isEnable: false,
         lineNumber: 2,
         rankText: {
-
+            1: "Reduces all spell damage taken by 2%.",
+            2: "Reduces all spell damage taken by 4%.",
+            3: "Reduces all spell damage taken by 6%.",
+            4: "Reduces all spell damage taken by 8%.",
+            5: "Reduces all spell damage taken by 10%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -98,7 +109,11 @@ let priestholy = [
         isEnable: false,
         lineNumber: 2,
         rankText: {
-
+            1: "Reduces the casting time of your Smite, Holy Fire, Heal and Greater Heal spells by 0.1 sec.",
+            2: "Reduces the casting time of your Smite, Holy Fire, Heal and Greater Heal spells by 0.2 sec.",
+            3: "Reduces the casting time of your Smite, Holy Fire, Heal and Greater Heal spells by 0.3 sec.",
+            4: "Reduces the casting time of your Smite, Holy Fire, Heal and Greater Heal spells by 0.4 sec.",
+            5: "Reduces the casting time of your Smite, Holy Fire, Heal and Greater Heal spells by 0.5 sec."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -128,7 +143,7 @@ let priestholy = [
         isEnable: false,
         lineNumber: 3,
         rankText: {
-
+            1: "Causes an explosion of holy light around the caster, causing 181 to 209 Holy damage to all enemy targets within 10 yards and healing all party members within 10 yards for 302 to 350. These effects cause no threat."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -143,7 +158,9 @@ let priestholy = [
         isEnable: false,
         lineNumber: 3,
         rankText: {
-
+            1: "After being struck by a melee or ranged critical hit, heal 8% of the damage taken over 6 sec.",
+            2: "After being struck by a melee or ranged critical hit, heal 16% of the damage taken over 6 sec.",
+            3: "After being struck by a melee or ranged critical hit, heal 25% of the damage taken over 6 sec."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -173,7 +190,9 @@ let priestholy = [
         isEnable: false,
         lineNumber: 3,
         rankText: {
-
+            1: "Increases your target's armor by 8% for 15 sec after getting a critical effect from your Flash Heal, Heal, Greater Heal, Binding Heal, Prayer of Healing, Prayer of Mending, or Circle of Healing spell.",
+            2: "Increases your target's armor by 16% for 15 sec after getting a critical effect from your Flash Heal, Heal, Greater Heal, Binding Heal, Prayer of Healing, Prayer of Mending, or Circle of Healing spell.",
+            3: "Increases your target's armor by 25% for 15 sec after getting a critical effect from your Flash Heal, Heal, Greater Heal, Binding Heal, Prayer of Healing, Prayer of Mending, or Circle of Healing spell."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -188,7 +207,8 @@ let priestholy = [
         isEnable: false,
         lineNumber: 4,
         rankText: {
-
+            1: "Increases the range of your Smite and Holy Fire spells and the radius of your Prayer of Healing, Holy Nova, and Circle of Healing spells by 10%.",
+            2: "Increases the range of your Smite and Holy Fire spells and the radius of your Prayer of Healing, Holy Nova, and Circle of Healing spells by 20%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -203,7 +223,9 @@ let priestholy = [
         isEnable: false,
         lineNumber: 4,
         rankText: {
-
+            1: "Reduces the Mana cost of your Lesser Heal, Heal, and Greater Heal spells by 5%.",
+            2: "Reduces the Mana cost of your Lesser Heal, Heal, and Greater Heal spells by 10%.",
+            3: "Reduces the Mana cost of your Lesser Heal, Heal, and Greater Heal spells by 15%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -218,7 +240,8 @@ let priestholy = [
         isEnable: false,
         lineNumber: 4,
         rankText: {
-
+            1: "Increases the damage of your Smite and Holy Fire spells by 5%.",
+            2: "Increases the damage of your Smite and Holy Fire spells by 10%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -248,7 +271,8 @@ let priestholy = [
         isEnable: false,
         lineNumber: 5,
         rankText: {
-
+            1: "Reduces the Mana cost of your Prayer of Healing and Prayer of Mending spells by 10%.",
+            2: "Reduces the Mana cost of your Prayer of Healing and Prayer of Mending spells by 20%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -263,7 +287,7 @@ let priestholy = [
         isEnable: false,
         lineNumber: 5,
         rankText: {
-
+            1: "Increases total Spirit by 5% and upon death, the priest becomes the Spirit of Redemption for 15 sec.  The Spirit of Redemption cannot move, attack, be attacked or targeted by any spells or effects.  While in this form, the priest can cast any healing spell free of cost.  When the effect ends, the priest dies."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -278,7 +302,11 @@ let priestholy = [
         isEnable: false,
         lineNumber: 5,
         rankText: {
-
+            1: "Increases spell damage and healing by up to 5% of your total Spirit.",
+            2: "Increases spell damage and healing by up to 10% of your total Spirit.",
+            3: "Increases spell damage and healing by up to 15% of your total Spirit.",
+            4: "Increases spell damage and healing by up to 20% of your total Spirit.",
+            5: "Increases spell damage and healing by up to 25% of your total Spirit."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -308,7 +336,9 @@ let priestholy = [
         isEnable: false,
         lineNumber: 6,
         rankText: {
-
+            1: "Your spell criticals have a 25% chance to cause your next Smite spell to be instant cast, cost no mana but be incapable of a critical hit. This effect lasts 10 sec.",
+            2: "Your spell criticals have a 50% chance to cause your next Smite spell to be instant cast, cost no mana but be incapable of a critical hit. This effect lasts 10 sec.",
+            3: "Your spell criticals have a 75% chance to cause your next Smite spell to be instant cast, cost no mana but be incapable of a critical hit. This effect lasts 10 sec."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -338,7 +368,11 @@ let priestholy = [
         isEnable: false,
         lineNumber: 6,
         rankText: {
-
+            1: "Increases the amount healed by your healing spells by 2%.",
+            2: "Increases the amount healed by your healing spells by 4%.",
+            3: "Increases the amount healed by your healing spells by 6%.",
+            4: "Increases the amount healed by your healing spells by 8%.",
+            5: "Increases the amount healed by your healing spells by 10%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -368,7 +402,9 @@ let priestholy = [
         isEnable: false,
         lineNumber: 7,
         rankText: {
-
+            1: "Gives you a 2% chance to enter a Clearcasting state after casting any Flash Heal, Binding Heal, or Greater Heal spell.  The Clearcasting state reduces the mana cost of your next Flash Heal, Binding Heal, or Greater Heal spell by 100%.",
+            2: "Gives you a 4% chance to enter a Clearcasting state after casting any Flash Heal, Binding Heal, or Greater Heal spell.  The Clearcasting state reduces the mana cost of your next Flash Heal, Binding Heal, or Greater Heal spell by 100%.",
+            3: "Gives you a 6% chance to enter a Clearcasting state after casting any Flash Heal, Binding Heal, or Greater Heal spell.  The Clearcasting state reduces the mana cost of your next Flash Heal, Binding Heal, or Greater Heal spell by 100%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -383,7 +419,7 @@ let priestholy = [
         isEnable: false,
         lineNumber: 7,
         rankText: {
-
+            1: "Creates a holy Lightwell.  Members of your raid or party can click the Lightwell to restore 2361 health over 6 sec. Any damage taken will cancel the effect.  Lightwell lasts for 3 min or 5 charges."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -398,7 +434,9 @@ let priestholy = [
         isEnable: false,
         lineNumber: 7,
         rankText: {
-
+            1: "Critical hits made against you have a 20% chance to prevent you from being critically hit again for 6 sec.",
+            2: "Critical hits made against you have a 40% chance to prevent you from being critically hit again for 6 sec.",
+            3: "Critical hits made against you have a 60% chance to prevent you from being critically hit again for 6 sec."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -443,7 +481,11 @@ let priestholy = [
         isEnable: false,
         lineNumber: 8,
         rankText: {
-
+            1: "Your Greater Heal spell gains an additional 4% and your Flash Heal and Binding Heal gains an additional 2% of your bonus healing effects.",
+            2: "Your Greater Heal spell gains an additional 8% and your Flash Heal and Binding Heal gains an additional 4% of your bonus healing effects.",
+            3: "Your Greater Heal spell gains an additional 12% and your Flash Heal and Binding Heal gains an additional 6% of your bonus healing effects.",
+            4: "Your Greater Heal spell gains an additional 16% and your Flash Heal and Binding Heal gains an additional 8% of your bonus healing effects.",
+            5: "Your Greater Heal spell gains an additional 20% and your Flash Heal and Binding Heal gains an additional 10% of your bonus healing effects."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -503,7 +545,7 @@ let priestholy = [
         isEnable: false,
         lineNumber: 9,
         rankText: {
-
+            1: "Heals friendly target and that target's party members within 15 yards of the target for 409 to 451."
         },
         isTalent: false,
         addRequirement: function() { return ""}
