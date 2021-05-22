f39ac7d7ed9c628f3b4495a8083f277d8a6fecec
Jose M Martinez <jonuel.martiguez@gmail.com>
2021-05-22 02:33:41 +0200

    warrior talent tree icons info

--- warrior/warriorfury.js
+++ warrior/warriorfury.js
@@ -23,7 +23,11 @@ let warriorfury = [
         isEnable: true,
         lineNumber: 1,
         rankText: {
-
+            1: "Increases the area of effect and duration of your Battle Shout, Demoralizing Shout, and Commanding Shout by 10%.",
+            2: "Increases the area of effect and duration of your Battle Shout, Demoralizing Shout, and Commanding Shout by 20%.",
+            3: "Increases the area of effect and duration of your Battle Shout, Demoralizing Shout, and Commanding Shout by 30%.",
+            4: "Increases the area of effect and duration of your Battle Shout, Demoralizing Shout, and Commanding Shout by 40%.",
+            5: "Increases the area of effect and duration of your Battle Shout, Demoralizing Shout, and Commanding Shout by 50%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -38,7 +42,11 @@ let warriorfury = [
         isEnable: true,
         lineNumber: 1,
         rankText: {
-
+            1: "Increases your chance to get a critical strike with melee weapons by 1%.",
+            2: "Increases your chance to get a critical strike with melee weapons by 2%.",
+            3: "Increases your chance to get a critical strike with melee weapons by 3%.",
+            4: "Increases your chance to get a critical strike with melee weapons by 4%.",
+            5: "Increases your chance to get a critical strike with melee weapons by 5%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -83,7 +91,11 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 2,
         rankText: {
-
+            1: "Increases the melee attack power reduction of your Demoralizing Shout by 8%.",
+            2: "Increases the melee attack power reduction of your Demoralizing Shout by 16%.",
+            3: "Increases the melee attack power reduction of your Demoralizing Shout by 24%.",
+            4: "Increases the melee attack power reduction of your Demoralizing Shout by 32%.",
+            5: "Increases the melee attack power reduction of your Demoralizing Shout by 40%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -98,7 +110,11 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 2,
         rankText: {
-
+            1: "Gives you a chance to generate an additional rage point when you deal melee damage with a weapon.",
+            2: "Gives you a chance to generate an additional rage point when you deal melee damage with a weapon. Effect occurs more often than Umbridled Wrath (Rank 1)",
+            3: "Gives you a chance to generate an additional rage point when you deal melee damage with a weapon. Effect occurs more often than Umbridled Wrath (Rank 2)",
+            4: "Gives you a chance to generate an additional rage point when you deal melee damage with a weapon. Effect occurs more often than Umbridled Wrath (Rank 3)",
+            5: "Gives you a chance to generate an additional rage point when you deal melee damage with a weapon. Effect occurs more often than Umbridled Wrath (Rank 4)"
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -128,7 +144,9 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 3,
         rankText: {
-
+            1: "Increases the bonus damage done by your Cleave ability by 40%.",
+            2: "Increases the bonus damage done by your Cleave ability by 80%.",
+            3: "Increases the bonus damage done by your Cleave ability by 120%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -143,7 +161,7 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 3,
         rankText: {
-
+            1: "Causes all enemies near the warrior to be dazed, reducing movement speed by 50% for 6 sec."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -158,7 +176,9 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 3,
         rankText: {
-
+            1: "Regenerates 1% of your total health over 6 sec after being the victim of a critical strike.",
+            2: "Regenerates 2% of your total health over 6 sec after being the victim of a critical strike.",
+            3: "Regenerates 3% of your total health over 6 sec after being the victim of a critical strike."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -173,7 +193,11 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 3,
         rankText: {
-
+            1: "Increases the melee attack power bonus of your Battle Shout and the health bonus of your Commanding Shout by 5%.",
+            2: "Increases the melee attack power bonus of your Battle Shout and the health bonus of your Commanding Shout by 10%.",
+            3: "Increases the melee attack power bonus of your Battle Shout and the health bonus of your Commanding Shout by 15%.",
+            4: "Increases the melee attack power bonus of your Battle Shout and the health bonus of your Commanding Shout by 20%.",
+            5: "Increases the melee attack power bonus of your Battle Shout and the health bonus of your Commanding Shout by 25%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -188,7 +212,11 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 4,
         rankText: {
-
+            1: "Increases the damage done by your offhand weapon by 5%.",
+            2: "Increases the damage done by your offhand weapon by 10%.",
+            3: "Increases the damage done by your offhand weapon by 15%.",
+            4: "Increases the damage done by your offhand weapon by 20%.",
+            5: "Increases the damage done by your offhand weapon by 25%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -203,7 +231,8 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 4,
         rankText: {
-
+            1: "Reduces the Rage cost of your Execute ability by 2.",
+            2: "Reduces the Rage cost of your Execute ability by 5."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -218,7 +247,11 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 4,
         rankText: {
-
+            1: "Gives you a 5% melee damage bonus for 12 sec up to a maximum of 12 swings after being the victim of a critical strike.",
+            2: "Gives you a 10% melee damage bonus for 12 sec up to a maximum of 12 swings after being the victim of a critical strike.",
+            3: "Gives you a 15% melee damage bonus for 12 sec up to a maximum of 12 swings after being the victim of a critical strike.",
+            4: "Gives you a 20% melee damage bonus for 12 sec up to a maximum of 12 swings after being the victim of a critical strike.",
+            5: "Gives you a 25% melee damage bonus for 12 sec up to a maximum of 12 swings after being the victim of a critical strike."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -248,7 +281,8 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 5,
         rankText: {
-
+            1: "Decreases the casting time of your Slam ability by 0.5 sec.",
+            2: "Decreases the casting time of your Slam ability by 1 sec."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -263,7 +297,7 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 5,
         rankText: {
-
+            1: "Your next 10 melee weapon swings strike an additional nearby opponent."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -293,7 +327,8 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 5,
         rankText: {
-
+            1: "Reduces the chance for you attacks to be dodged by 1% and reduces the duration of all Disarm effects used against you by 25%. This does not stack with other Disarm duration reducing effects.",
+            2: "Reduces the chance for you attacks to be dodged by 2% and reduces the duration of all Disarm effects used against you by 50%. This does not stack with other Disarm duration reducing effects."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -308,7 +343,8 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 6,
         rankText: {
-
+            1: "The Berserker Rage ability will generate 5 rage when used.",
+            2: "The Berserker Rage ability will generate 10 rage when used."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -338,7 +374,11 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 6,
         rankText: {
-
+            1: "Increases your attack speed by 5% for your next 3 swings after dealing a melee critical strike.",
+            2: "Increases your attack speed by 10% for your next 3 swings after dealing a melee critical strike.",
+            3: "Increases your attack speed by 15% for your next 3 swings after dealing a melee critical strike.",
+            4: "Increases your attack speed by 20% for your next 3 swings after dealing a melee critical strike.",
+            5: "Increases your attack speed by 25% for your next 3 swings after dealing a melee critical strike."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -368,7 +408,9 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 7,
         rankText: {
-
+            1: "Increases your chance to hit with melee weapons by 1%.",
+            2: "Increases your chance to hit with melee weapons by 2%.",
+            3: "Increases your chance to hit with melee weapons by 3%."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -383,7 +425,7 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 7,
         rankText: {
-
+            1: "Instantly attack the target causing 217 damage. In addition, the next 5 successful melee attacks will restore 80 health. This effect lasts 8 sec. Damage is based on your attack power."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -398,7 +440,8 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 7,
         rankText: {
-
+            1: "Reduces the cooldown of your Whirlwind ability by 1 sec.",
+            2: "Reduces the cooldown of your Whirlwind ability by 2 sec."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -458,7 +501,11 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 8,
         rankText: {
-
+            1: "Increases attack power by 2% and reduces threat caused by 2% while in Berserker Stance.",
+            2: "Increases attack power by 4% and reduces threat caused by 4% while in Berserker Stance.",
+            3: "Increases attack power by 6% and reduces threat caused by 6% while in Berserker Stance.",
+            4: "Increases attack power by 8% and reduces threat caused by 8% while in Berserker Stance.",
+            5: "Increases attack power by 10% and reduces threat caused by 10% while in Berserker Stance."
         },
         isTalent: false,
         addRequirement: function() { return ""}
@@ -503,7 +550,7 @@ let warriorfury = [
         isEnable: false,
         lineNumber: 9,
         rankText: {
-
+            1: "Warrior goes on a rampage, increasing attack power by 30 and causing most successful melee attacks to increase attack power by an additional 50. This effect will stack up to 5 times. Lasts 30 sec. This ability can only be used after scoring a critical hit."
         },
         isTalent: false,
         addRequirement: function() { return ""}
